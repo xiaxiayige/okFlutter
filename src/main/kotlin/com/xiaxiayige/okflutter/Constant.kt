0be0b3ca06693c2654c73f3e0d9a8f3c898c4b2c
zhangqilin <xiaxiayige@163.com>
2020-12-07 11:02:54 +0800

    change timeout 60--->30

--- src/main/kotlin/com/xiaxiayige/okflutter/Constant.kt
+++ src/main/kotlin/com/xiaxiayige/okflutter/Constant.kt
@@ -12,7 +12,7 @@ object Constant {
     val HEADER_USER_AGENT_VALUE =
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
     val QUERY_SORT = "&sort="
-    val TIME_OUT = 1000 * 60
+    val TIME_OUT = 1000 * 30
 
     const val DEPENDENCY_COPIED_TITLE = "Copied!"
     const val DEPENDENCY_COPIED_MSG = "Dependency statements have been copied to your clipboard."
